5dcb7052f7a00a91bb0c4a10e19cf3cc38d0234a
juliet879 <gisembajuliett@gmail.com>
2021-06-11 19:18:52 +0300

    created class MobileMoneyAccount to inherit from class Account.One is know able to buy airtime

--- bank.py
+++ bank.py
@@ -14,6 +14,11 @@ class Account:
         self.transactions = []
 
     def deposit(self,amount):
+        try:
+            amount + 10
+        except TypeError:
+            return f"Please enter amount in figures"
+
         if amount<=0:
             return f"please deposit a valid amount"
 
@@ -23,9 +28,14 @@ class Account:
             self.transactions.append(transaction)
             return f"Hello, {self.name} you have deposited {amount} ,new balance is {self.balance}"
     
-
+       
      
     def withdraw(self,withdrawal_amount):
+        try:
+            withdrawal_amount + 10
+        except TypeError:
+            return f"Please enter amount in figures"
+
         total = withdrawal_amount + self.transaction_fee
         if withdrawal_amount <= 0:
             return "please enter valid amount"
@@ -41,6 +51,11 @@ class Account:
             return f"Hello {self.name} you have successfully withdrawn {withdrawal_amount} account balance is {self.balance}"
 
     def borrow(self,loan):
+        try:
+            loan + 10
+        except TypeError:
+            return f"Please enter amount in figures"
+
         loan_fees = self.loan_interest * loan / 100
         
         if loan < 0:
@@ -61,6 +76,11 @@ class Account:
             return f"You have received a loan of {loan} your account balance now is {self.balance},the amount you will repay is {self.loan}"
 
     def repay(self,repay_amount):
+        try:
+            repay_amount + 10
+        except TypeError:
+            return f"Please enter amount in figures"
+
         if repay_amount < 0:
             return "Please enter a valid amount"
 
@@ -71,11 +91,37 @@ class Account:
             return f"You have repaid {repay_amount} outstanding loan is {self.loan}"
 
         elif repay_amount > self.loan:
+            self.loan = 0
             excess = repay_amount - self.loan
             self.balance += excess
             transaction = {"amount":repay_amount,"balance":self.balance,"narration":"You repaid loan","time":dt.now()}
             self.transactions.append(transaction)
             return f"You have fully settled your loan, account balance is {self.balance}"
+
+        
+    def transfer(self,transfer_amount,acc):
+
+        try:
+            transfer_amount + 10
+        except TypeError:
+            return f"Please enter amount in figures"
+        
+        fee = transfer_amount * 5/100
+        total = fee + transfer_amount
+
+        if transfer_amount < 0 :
+            return "Please enter a valid amount"
+
+        elif total > self.balance:
+            return f"You have an insufficient balance of {self.balance}"
+
+        else:
+            self.balance -= total
+            acc.deposit(transfer_amount)
+            return f"You have succesfully transfered {transfer_amount} to {acc.name},current account balance is {self.balance}"
+
+
+
 
     def get_statement(self):
         for transaction in self.transactions:
@@ -86,5 +132,28 @@ class Account:
             date = time.strftime("%D")
             print(f"{date}...{narration} {amount}.....Balance {balance}")
 
-            
+class MobileMoneyAccount(Account):
+    def __init__(self, name, phone,service_provider):
+        Account.__init__(self,name, phone)
+        self.service_provider = service_provider     
+
+    def buy_airtime(self,amount):
+
+        try:
+            amount + 10
+        except TypeError:
+            return f"Please enter amount in figures"
+        
+        if amount < 0:
+            return "Please enter a valid amount"
+
+        elif amount > self.balance:
+            return "Insufficient balance"
+
+        else:
+            self.balance -= amount
+            transaction = {"amount":amount,"narration":"Bought Airtime","balance":self.balance,"time":dt.now()}
+            self.transactions.append(transaction)
+            return f"You have successfully bought airtime of {amount} account balance is {self.balance}" 
+          
 
